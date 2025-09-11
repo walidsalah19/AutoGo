84e6ee7e5c7e80491c8a29d36195e33c4813efcf
walid salah <walidsalah9585@gmail.com>
2025-09-11 03:03:51 +0300

    dbset

---
+++ AutoGo.Application/Common/Context/IAppDbContext.cs
@@ -0,0 +1,27 @@
+﻿using AutoGo.Domain.Models;
+using ClinicalManagement.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoGo.Application.Common.Context
+{
+    public interface IAppDbContext
+    {
+        public DbSet<Dealer> Dealers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<MaintenanceRecord> MaintenanceRecords { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<ServiceOrder> ServiceOrders { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleImage> VehicleImages { get; set; }
+        public DbSet<Workshop> Workshops { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+    }
+}
---
+++ AutoGo.Infrastructure/Data/Context/AppDbContext.cs
@@ -0,0 +1,46 @@
+﻿using AutoGo.Application.Common.Context;
+using AutoGo.Domain.Models;
+using ClinicalManagement.Domain.Models;
+using ClinicalManagement.Infrastructure.Data.Configrations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoGo.Infrastructure.Data.Context
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>,IAppDbContext
+    {
+        public DbSet<Dealer> Dealers { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<MaintenanceRecord> MaintenanceRecords { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<Rental> Rentals { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<ServiceOrder> ServiceOrders { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<VehicleImage> VehicleImages { get; set; }
+        public DbSet<Workshop> Workshops { get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(typeof(RefreshTokenConfigration).Assembly);
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
