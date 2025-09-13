912dd2bcb0b2c47d64d8e2f49fbfd8d93cac80c9
walid salah <walidsalah9585@gmail.com>
2025-09-13 17:54:36 +0300

    extentions

---
+++ AutoGo.Api/Extentions/ApiExtentintions.cs
@@ -0,0 +1,48 @@
+﻿using Serilog;
+
+namespace AutoGo.Api.Extentions
+{
+    public static class ApiExtentintions
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            //عشان نتبع ال endpoint والوقت بتاعها 
+            services.AddMiniProfiler(options =>
+            {
+                options.RouteBasePath = "/profiler";
+                options.ColorScheme = StackExchange.Profiling.ColorScheme.Auto;
+                options.TrackConnectionOpenClose = true;
+            }).AddEntityFramework();
+
+
+            //استخدام ال serilog عشان نعمل logging 
+            Log.Logger = new LoggerConfiguration()
+                        .MinimumLevel.Debug()
+                        .WriteTo.Console() // عرض في الكونسول
+                        .WriteTo.Seq("http://localhost:5341") // رابط الـ Seq
+                        .Enrich.FromLogContext()
+                        .Enrich.WithEnvironmentName()
+                        .Enrich.WithMachineName()
+                        .Enrich.WithThreadId()
+                        .CreateLogger();
+
+
+            //cors policy
+            services.AddCors(options =>
+            {
+                options.AddPolicy("default", policy =>
+                {
+                    policy.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+
+
+            // segnalr
+            services.AddSignalR();
+            return services;
+        }
+
+    }
+}
---
+++ AutoGo.Api/Extentions/AuthenticationServices.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AutoGo.Api.Extentions
+{
+    public static class AuthenticationServices
+    {
+        public static IServiceCollection AddAuthServices(this IServiceCollection services,IConfiguration configuration)
+        {
+
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false; 
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = configuration["JWT:IssuerIP"],
+                    ValidateAudience = true,
+                    ValidAudience = configuration["JWT:AudienceIP"],
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(
+                            System.Text.Encoding.UTF8.GetBytes(configuration["JWT:SecritKey"]))
+
+                };
+            });
+
+            return services;
+        }
+    }
+}
